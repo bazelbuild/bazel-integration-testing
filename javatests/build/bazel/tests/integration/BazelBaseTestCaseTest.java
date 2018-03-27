62e50b05302e436d5517eecb592c41bae750748f
Maxim Novak <maximn@wix.com>
2018-03-27 21:29:04 +0300

    no wildcard import

--- javatests/build/bazel/tests/integration/BazelBaseTestCaseTest.java
+++ javatests/build/bazel/tests/integration/BazelBaseTestCaseTest.java
@@ -23,7 +23,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
