5c7d5f57adf41e13e1143b0c4309bcba1f4012fe
Ittai Zeidman <ittaiz@gmail.com>
2018-03-30 08:46:39 +0300

    removed unused import

--- javatests/build/bazel/tests/integration/WorkspaceDriverTest.java
+++ javatests/build/bazel/tests/integration/WorkspaceDriverTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
