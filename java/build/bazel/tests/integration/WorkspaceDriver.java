a5d96a2745f11d2d65c975e330fe2983e7b847e6
Shachar Anchelovich <shacharan@wix.com>
2018-04-04 16:02:30 +0300

    Revert wildcard imports

--- java/build/bazel/tests/integration/WorkspaceDriver.java
+++ java/build/bazel/tests/integration/WorkspaceDriver.java
@@ -19,7 +19,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
